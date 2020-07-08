c6f7690305254c7c0f5c62b536e6c04cf141f4e0
Taemin Kim <kofgg8541@gmail.com>
2020-07-08 17:22:50 +0900

    test
    test

---
+++ searchParams.js
@@ -0,0 +1,7 @@
+const {URL}  = require('url');
+
+const myURL = new URL('http://gilbut.co.kr/?page=3&limit=10&category=nodejs&category=javascript');
+console.log('searchParams:', myURL.searchParams);
+console.log('searchParams.getAll()' : myURL.searchParams.getAll('category'));
+console.log('',searchParams.get('limit'));
+console.log('' )

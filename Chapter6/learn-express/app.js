ddfc80f620171af5edb38db26939003bd62f83b2
Kims_Bucket <kofgg8541@gmail.com>
2020-07-15 11:01:08 +0900

    충돌용

--- Chapter6/learn-express/app.js
+++ Chapter6/learn-express/app.js
@@ -3,29 +3,56 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// app.use(function(req, res, next) {
+//   console.log(req.url, '저도 미들웨어 입니다.');
+//   next();
+// });
+
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser());
+app.use(cookieParser('secret code')); // 서명된 쿠키가 있는경우 제공한 문자열을 키로 삼아 복호화 가능 클라이언트가 서명된 쿠키를 임의로 수정할경우 클라는 에러 발생
+
+//expression-session사용
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -37,5 +64,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;

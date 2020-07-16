6cd93e5a60a883cdf8ff19b7c2d3bf3e8e5d855f
Taemin Kim <kofgg8541@gmail.com>
2020-07-16 10:19:08 +0900

    chapter6

--- Chapter6/learn-express/app.js
+++ Chapter6/learn-express/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -14,10 +17,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('secret code'));
+app.use((session)({
+  resave: false,
+  saveUninitialized: false,
+  secret : 'cookie code',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+app.use(flash());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- Chapter6/learn-express/routes/users.js
+++ Chapter6/learn-express/routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
+const app = require('../app');
 var router = express.Router();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// //params와 query객체로 값들이 들어옴
+// //다양한 아우터중 와일드카드 역할 이므로 일반 라우터들보다 뒤에 배치해야하 방해하지않음
+// router.get('/:id', function(req, res) {
+//   console.log(req.params, req.query);
+//   res.send('respond with a resource');
+// });
+
+
+
+router.get('/flash', function(req, res){
+  req.session.message = '세션 메시지';
+  req.flash('message', 'flash 메시지');
+  res.redirect('/users/flash/result');
+});
+
+router.get('/flash/result', function(req,res){
+  res.send(`${req.session.message} ${req.flash('message')}`);
+});
+
+
+
+
+
 module.exports = router;
+
+

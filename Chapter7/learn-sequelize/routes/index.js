a3c5e5c9f57d47d989314930ebcb914bdb713a12
Kims_Bucket <kofgg8541@gmail.com>
2020-07-20 11:20:26 +0900

    chapter6End

--- Chapter7/learn-sequelize/routes/index.js
+++ Chapter7/learn-sequelize/routes/index.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var User = require('../models').User;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// var router = express.Router();
+
+// /* GET home page. */
+
+// //async/await 문법 사용
+// router.get('/', async(req, res, next) => {
+
+//   try {
+//     const users = await User.findALL();
+//     res.render('sequelize', {users});
+//   } catch (error) {
+//     console.error(err);
+//     next(err);
+//   }
+
+// });
+
+
+// // router.get('/', function(req, res, next) {
+// //   User.findAll()
+// //   .then((users) => {
+// //     res.render('sequelize', {users});
+// //   })
+// //   .catch((err) => {
+// //     console.error(err);
+// //     next(err);
+// //   });
+// // });
+
+
+
+// module.exports = router;
+
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    console.log(users);
+    res.render('sequelize', { users });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
